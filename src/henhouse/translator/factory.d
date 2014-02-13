d3d44be41f0b150a2a12d667c7296ece9c724e28
Marc Neudert <marc.neudert@gmail.com>
2014-02-13 21:01:07 +0100

    ignores links in markdown input files

--- src/henhouse/translator/factory.d
+++ src/henhouse/translator/factory.d
@@ -1,10 +1,30 @@
 module henhouse.translator.factory;
 
+import str = std.string;
+
 import hht_i = henhouse.translator.itranslator;
+import hht_m = henhouse.translator.markdown;
 import hht_t = henhouse.translator.text;
 
 
 hht_i.ITranslator getTranslator(string filename)
 {
-    return new hht_t.Translator();
+    string ext;
+
+    long posSlash = str.lastIndexOf(filename, '/');
+    long posDot   = str.lastIndexOf(filename, '.');
+
+    if (0 < posDot && posSlash < posDot) {
+        ext = filename[(posDot + 1) .. $];
+    }
+
+    switch (str.toLower(ext))
+    {
+        case "md":
+        case "markdown":
+            return new hht_m.Translator();
+
+        default:
+            return new hht_t.Translator();
+    }
 }
---
+++ src/henhouse/translator/markdown.d
@@ -0,0 +1,69 @@
+module henhouse.translator.markdown;
+
+import file = std.file;
+import uni  = std.uni;
+
+import hhc   = henhouse.converter;
+import hht_t = henhouse.translator.text;
+
+
+class Translator : hht_t.Translator
+{
+    override string translateText(string original)
+    {
+        char   current;
+        string chicken;
+        string buffer;
+
+        for (int i = 0; i < original.length; i++) {
+            current = original[i];
+
+            if ('[' == current) {
+                do {
+                    chicken ~= original[i];
+                    i++;
+                }
+                while (']' != original[i]);
+
+                chicken ~= ']';
+
+                if ('(' == original[i + 1]) {
+                    i++;
+
+                    do {
+                        chicken ~= original[i];
+                        i++;
+                    }
+                    while (')' != original[i]);
+
+                    chicken ~= ')';
+                }
+
+                continue;
+            }
+
+            if (uni.isAlpha(current)) {
+                buffer ~= current;
+                continue;
+            }
+
+            if (0 < buffer.length) {
+                chicken ~= hhc.wordToChicken(buffer);
+                buffer   = "";
+            }
+
+            chicken ~= current;
+        }
+
+        return chicken;
+    }
+
+    unittest
+    {
+        Translator trans = new Translator();
+        string original  = "Very Markdown! [uncoverted](link)";
+        string chicken   = "Chicken Chicken! [uncoverted](link)";
+
+        assert(chicken == trans.translateText(original));
+    }
+}
